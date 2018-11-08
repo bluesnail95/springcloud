04d55befd3853ca9704916deea00dc1b21b78df6
bluesnail95 <13642315483@163.com>
2018-11-08 23:30:37 +0800

    测试redis集群的连接

--- sso/src/test/java/gdut/ff/RedisTest.java
+++ sso/src/test/java/gdut/ff/RedisTest.java
@@ -1,28 +1,51 @@
 package gdut.ff;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import redis.clients.jedis.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = Main.class)
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class RedisTest {
-
-    @Autowired
-    private RedisTemplate redisTemplate;
 
     @Test
     public void testSetString() {
-        redisTemplate.opsForValue().set("hello","world");
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(10);
+        config.setMaxIdle(5);
+        config.setMaxWaitMillis(3000);
+        config.setTestOnBorrow(true);
+        config.setTestOnReturn(true);
 
-        String value = (String) redisTemplate.opsForValue().get("E439A3BA123466AF79F556F269AD9093");
+        Set<HostAndPort> set = new HashSet<HostAndPort>();
+        set.add(new HostAndPort("139.199.210.171",6380));
+        set.add(new HostAndPort("139.199.210.171",6381));
+        set.add(new HostAndPort("139.199.210.171",6382));
+        set.add(new HostAndPort("139.199.210.171",6383));
+        set.add(new HostAndPort("139.199.210.171",6384));
+        set.add(new HostAndPort("139.199.210.171",6385));
 
+        JedisCluster cluster = new JedisCluster(set,15000,10000,10,"19950821abc",config);
+        cluster.setnx("hello","tomorrow will be better");
+        String result = cluster.get("hello");
+        System.out.println(result);
+        try {
+            cluster.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Test
+    public void test1() {
+        Jedis jedis = new Jedis("139.199.210.171",6381,20000);
+        jedis.auth("19950821abc");
+        jedis.set("hello","world");
+        System.out.println(jedis.get("hello"));
 
-        System.out.println(value);
     }
 
 }
