0bdeec2aa10de51ede3036ad1a8cf7ed4bb9d0c2
bluesnail95 <13642315483@163.com>
2019-01-21 23:52:02 +0800

    添加api-gateway-dynamic-route

--- api-gateway-dynamic-route/src/main/java/gdut/ff/Main.java
+++ api-gateway-dynamic-route/src/main/java/gdut/ff/Main.java
@@ -2,26 +2,36 @@ package gdut.ff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
 
 /**
- * 2019-01-20
+ * 2019-01-21
  * liuffei
  */
-//@ComponentScan(basePackages = "gdut.ff")
-//@EnableAutoConfiguration
+@ComponentScan(basePackages = "gdut.ff")
+@EnableAutoConfiguration
 @EnableZuulProxy
-@SpringCloudApplication
 public class Main {
 
+    public static void main(String args[]) {
+        new SpringApplicationBuilder(Main.class).web(true).run(args);
+    }
 
-//    public static void main(String args[]) {
-//        SpringApplication.run(Main.class, args);
-//    }
-
+    @Bean
+    @RefreshScope
+    @ConfigurationProperties("zuul")
+    @Primary
+    public ZuulProperties zuulProperties() {
+        return new ZuulProperties();
+    }
 
 }
